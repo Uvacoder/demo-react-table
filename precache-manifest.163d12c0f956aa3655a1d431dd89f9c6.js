5004458d1e8f793bc9d747409d38f72a923c8853
mdlufy <german.panov.2018@mail.ru>
2022-09-21 19:12:02 +0300

    Updates

---
+++ precache-manifest.163d12c0f956aa3655a1d431dd89f9c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb20ac7a4410976bd84",
+    "url": "/react-table/static/css/main.e840f9c1.chunk.css"
+  },
+  {
+    "revision": "3fb20ac7a4410976bd84",
+    "url": "/react-table/static/js/main.fbab2085.chunk.js"
+  },
+  {
+    "revision": "dad51b5cb92572d43e00",
+    "url": "/react-table/static/js/runtime~main.15ef8938.js"
+  },
+  {
+    "revision": "d9d17ecfdea7f55332c6",
+    "url": "/react-table/static/js/2.2ff42289.chunk.js"
+  },
+  {
+    "revision": "e0a632621e3bb44ff90489c3b338718e",
+    "url": "/react-table/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-table/precache-manifest.eb8835395323cc68e52ef28cbde55320.js"
+  "/react-table/precache-manifest.163d12c0f956aa3655a1d431dd89f9c6.js"
 );
 
 workbox.clientsClaim();
